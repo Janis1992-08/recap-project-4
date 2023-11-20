5987e1767c56fa839d260b97835d8a562de54c2c
Janis Schiller <schillerjanis@gmail.com>
2023-11-20 11:19:21 +0100

    update form-element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import {Form} from "./component/form/form.js"
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Form /> 
     </div>
   ); 
 }
--- src/component/form/form.js
+++ src/component/form/form.js
@@ -1,1 +1,60 @@
-import './form.css';
+
+import { useState } from 'react';
+
+
+
+
+export function Form({ onAddActivity }) {
+    const [activity, setActivity] = useState('');
+    const [isGoodWeather, setIsGoodWeather] = useState(false)
+
+      const handleActivityChange = (event) => {
+        setActivity(event.target.value);
+      };
+    
+      const handleWeatherChange = (event) => {
+        setIsGoodWeather(event.target.checked);
+      };
+    
+      const handleSubmit = (event => {
+        event.preventDefault();
+        const newActivity = {
+          name: activity,
+          isGoodWeather: isGoodWeather,
+        };
+        onAddActivity(newActivity);
+        setActivity('');
+        setIsGoodWeather(false);
+      });
+    
+    return (
+        <form onSubmit={handleSubmit}>
+          <h2>Activity Form</h2>
+          <div>
+            <label htmlFor="activity">Name of Activity:</label>
+            <input
+              type="text"
+              id="activity"
+              value={activity}
+              onChange={handleActivityChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="check">
+              Good Weather Activity: 
+              </label>
+              <input
+              id="check"
+                type="checkbox"
+                checked={isGoodWeather}
+                onChange={handleWeatherChange}
+              />
+          </div>
+          <button type="submit">Submit</button>
+        </form>);
+
+
+
+
+};
+
