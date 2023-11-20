5ed7c68ef4a9339ce894ee5cc8c65ff179bad051
Alexander Kolotow <alexkolotow@gmail.com>
2023-11-20 15:48:09 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Form } from "./component/form/index.js"
-import {List} from "./component/list/index.js"
+import { List } from "./component/list/index.js"
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 import useLocalStorageState from "use-local-storage-state";
@@ -7,7 +7,7 @@ import useLocalStorageState from "use-local-storage-state";
 
 function App() {
 const [activities, setActivities] = useLocalStorageState("activities", {defaultValue: []});
-// maybe we need this? 
+
 const handleAddActivity = (newActivity) => {
   const activityWithId = {...newActivity, id: uuidv4()};
  console.log(newActivity); setActivities( [...activities, activityWithId]);
