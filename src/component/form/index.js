95d76538e2efe3bee4e5a3a4d7c9e061cc2a70bd
Alexander Kolotow <alexkolotow@gmail.com>
2023-11-21 12:33:17 +0100

    add inline css styling

--- src/component/form/index.js
+++ src/component/form/index.js
@@ -36,23 +36,35 @@ export function Form({ onAddActivity, setIsGoodWeather }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Activity Form</h2>
-      <div>
-        <label htmlFor="activity">Name of Activity:</label>
-        <input type="text" id="activity" value={activity} onChange={handleActivityChange} />
-      </div>
-      <div>
-        <label htmlFor="check">Good Weather Activity:</label>
-        <input
-          id="check"
-          type="checkbox"
-          name="check"
-          checked={isGoodWeatherCheckbox}
-          onChange={handleWeatherChange}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
+    <form onSubmit={handleSubmit} style={{ fontFamily: 'Arial, sans-serif', maxWidth: '300px', margin: '0 auto' }}>
+  <h2 style={{ textAlign: 'center' }}>Activity Form</h2>
+  <div style={{ marginBottom: '10px' }}>
+    <label htmlFor="activity" style={{ display: 'block' }}>Name of Activity:</label>
+    <input
+      type="text"
+      id="activity"
+      value={activity}
+      onChange={handleActivityChange}
+      style={{ width: '100%', padding: '5px', borderRadius: '3px', border: '1px solid #ccc' }}
+    />
+  </div>
+  <div style={{ marginBottom: '10px' }}>
+    <label htmlFor="check" style={{ display: 'block' }}>Good Weather Activity:</label>
+    <input
+      id="check"
+      type="checkbox"
+      name="check"
+      checked={isGoodWeatherCheckbox}
+      onChange={handleWeatherChange}
+      style={{ marginRight: '5px' }}
+    />
+  </div>
+  <button
+    type="submit"
+    style={{ backgroundColor: '#007bff', color: '#fff', padding: '8px 15px', borderRadius: '3px', border: 'none' }}
+  >
+    Submit
+  </button>
+</form>
+  )
 }
--- src/component/list/index.js
+++ src/component/list/index.js
@@ -2,15 +2,41 @@ export function List({ activities, isGoodWeather, onDeleteActivity }) {
 
   return (
     <div>
-      <h3> {isGoodWeather ? "The weather is awesome! Go outside and: " : "Bad weather outside! Here's what you can do now: "}</h3>
-      <ul>
-        {activities.map((activity) => (
-          <li key={activity.id}>
-            {activity.name}
-            <button onClick={() => onDeleteActivity(activity.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+     <h3 style={{ color: isGoodWeather ? '#28a745' : '#dc3545' }}>
+  {isGoodWeather ? "The weather is awesome! Go outside and: " : "Bad weather outside! Here's what you can do now: "}
+</h3>
+<ul style={{ listStyleType: 'none', padding: 0, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+  {activities.map((activity) => (
+    <li
+      key={activity.id}
+      style={{
+        marginBottom: '10px',
+        padding: '10px',
+        borderRadius: '5px',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        backgroundColor: '#f7f7f7',
+        width: '20%',
+        textAlign: 'center'
+      }}
+    >
+      <span style={{ marginRight: '10px' }}>{activity.name}</span>
+      <button
+        onClick={() => onDeleteActivity(activity.id)}
+        style={{
+          padding: '5px 10px',
+          borderRadius: '3px',
+          backgroundColor: '#007bff',
+          color: '#fff',
+          border: 'none',
+          cursor: 'pointer'
+        }}
+      >
+        Delete
+      </button>
+    </li>
+  ))}
+</ul>
+
     </div>
   );
 }
