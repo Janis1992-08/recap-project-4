613f86eb94011c3247cbb1fa67695d66e4f1581f
Alexander Kolotow <alexkolotow@gmail.com>
2023-11-21 11:44:00 +0100

    add delete button for items

--- src/component/list/index.js
+++ src/component/list/index.js
@@ -1,11 +1,13 @@
-export function List({ activities, isGoodWeather }) {
+export function List({ activities, isGoodWeather, onDeleteActivity }) {
+
   return (
     <div>
       <h3> {isGoodWeather ? "The weather is awesome! Go outside and: " : "Bad weather outside! Here's what you can do now: "}</h3>
       <ul>
-        {activities.map((activity, index) => (
-          <li key={index}>
+        {activities.map((activity) => (
+          <li key={activity.id}>
             {activity.name}
+            <button onClick={() => onDeleteActivity(activity.id)}>Delete</button>
           </li>
         ))}
       </ul>
